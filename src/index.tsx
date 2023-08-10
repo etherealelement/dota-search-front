f6988d6a10a04a4c08539878dcffed36750cf3c0
Alexander <51529891+Truenya@users.noreply.github.com>
2023-08-10 14:02:31 +0300

    feat: esbuild

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import App from './components/app';
+
+import {createRoot} from 'react-dom/client';
+import React from 'react';
+
+const root = createRoot(
+    document.getElementById('root')
+);
+root.render(<App/>);
